de51e562e56769d3374f80a01e5eb2728424f3e8
VoidTycoonDev <>
2026-01-08 23:59:05 +0500

    Set target FPS to 165

--- src/main.js
+++ src/main.js
@@ -62,6 +62,10 @@ async function initApp() {
                 pixelArt: true,
                 roundPixels: true
             },
+            fps: {
+                target: 165,
+                forceSetTimeOut: false
+            },
             scene: [BootScene, GameScene]
         };
 
