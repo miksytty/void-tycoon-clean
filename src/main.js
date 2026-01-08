df5cff74ac236845781743b5f3c5579980f11733
VoidTycoonDev <>
2026-01-08 23:56:11 +0500

    Unlock FPS back to 60

--- src/main.js
+++ src/main.js
@@ -62,10 +62,6 @@ async function initApp() {
                 pixelArt: true,
                 roundPixels: true
             },
-            fps: {
-                target: 30,
-                forceSetTimeOut: true
-            },
             scene: [BootScene, GameScene]
         };
 
